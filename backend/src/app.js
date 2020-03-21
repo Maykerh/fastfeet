08e8cea799e2be27d90ee730545a91fa46f37561
Mayke <make_zs@hotmail.com>
2020-03-21 01:34:15 -0300

    Adicionado cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import routes from "./routes";
+import cors from "cors";
 
 import "./database";
 
@@ -13,6 +14,7 @@ class App {
     }
 
     middlewares() {
+        this.server.use(cors());
         this.server.use(express.json());
     }
 
