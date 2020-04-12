451b46d864ffe35d69532e042fd2aaa0b3af996b
Mayke <make_zs@hotmail.com>
2020-04-12 20:21:52 -0300

    criado arquivo env

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    define: {
+        timestamp: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,5 +1,4 @@
 import Sequelize from "sequelize";
-import mongoose from "mongoose";
 
 import User from "../app/models/User";
 import Recipient from "../app/models/Recipient";
@@ -15,7 +14,6 @@ const models = [User, Recipient, Deliveryman, Order, File, DeliveryProblems];
 class Database {
     constructor() {
         this.init();
-        this.mongo();
     }
 
     init() {
@@ -24,14 +22,6 @@ class Database {
         models
             .map(model => model.init(this.connection))
             .map(model => model.associate && model.associate(this.connection.models));
-    }
-
-    mongo() {
-        this.mongoConnection = mongoose.connect("mongodb://localhost:27017/fastfeet", {
-            useNewUrlParser: true,
-            useFindAndModify: true,
-            useUnifiedTopology: true,
-        });
     }
 }
 
